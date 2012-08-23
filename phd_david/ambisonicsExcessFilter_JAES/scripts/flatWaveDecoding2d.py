de70f2bd9121e0c831cb5dce1093998881b7dbcd
David García Garzón <voki@canvoki.net>
2012-08-23 05:57:13 +0200

    flatWaveDecoding2d: uses latex subindexes

--- phd_david/ambisonicsExcessFilter_JAES/scripts/flatWaveDecoding2d.py
+++ phd_david/ambisonicsExcessFilter_JAES/scripts/flatWaveDecoding2d.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+"""\
+Creates a polar plot with the gains of a 2d ambisonics decoding of a plane wave
+at a given order and decoding criteria.
+
+Usage: ./flatWaveDecoding2d.py maxre|maxrv|inphase [order1 [ order2 [ order3 ... ] ] ]
+"""
+
 import math
 import os
 import bmaudio
@@ -6,8 +13,6 @@ import pylab
 import sys
 from parameters import *
 import headDistortion
-
-ordersToShow=[1,3,9,35]
 
 decodings=dict(
 	inphase = headDistortion.inphasePolarPattern,
@@ -20,10 +25,13 @@ if len(sys.argv)<2 or sys.argv[1] not in decodings.keys() :
 decodingName=sys.argv[1]
 polarPattern = decoding=decodings[decodingName]
 title = dict(
-	maxre = "Max Re",
-	maxrv = "Max Rv",
+	maxre = "Max $R_E$",
+	maxrv = "Max $R_V$",
 	inphase = "In-phase",
 	)[decodingName]
+
+ordersToShow = [1,3,9,35]
+if  sys.argv[-1:1:-1] : ordersToShow = [int(a) for a in sys.argv[2:]]
 
 def figurePath(file, infix, extension) :
 	return os.path.join('figures', os.path.splitext(os.path.basename(file))[0] + infix + "." + extension)
