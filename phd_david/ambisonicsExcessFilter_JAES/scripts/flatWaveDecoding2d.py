a76163fa9c42f9d45657cbc4591210b4cb9e1c43
David García Garzón <voki@canvoki.net>
2012-08-23 05:17:54 +0200

    flatWaveDecodingXXXX -> flatWaveDecoding2d

---
+++ phd_david/ambisonicsExcessFilter_JAES/scripts/flatWaveDecoding2d.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import math
+import os
+import bmaudio
+import pylab
+import sys
+from parameters import *
+import headDistortion
+
+ordersToShow=[1,3,9,35]
+
+decodings=dict(
+	inphase = headDistortion.inphasePolarPattern,
+	maxre = headDistortion.maxrePolarPattern,
+	maxrv = headDistortion.basicPolarPattern,
+	)
+if len(sys.argv)<2 or sys.argv[1] not in decodings.keys() :
+	print "Please specify one decoding:", ", ".join(decodings.keys())
+	sys.exit()
+decodingName=sys.argv[1]
+polarPattern = decoding=decodings[decodingName]
+title = dict(
+	maxre = "Max Re",
+	maxrv = "Max Rv",
+	inphase = "In-phase",
+	)[decodingName]
+
+def figurePath(file, infix, extension) :
+	return os.path.join('figures', os.path.splitext(os.path.basename(file))[0] + infix + "." + extension)
+
+N*=2
+azimuths = numpy.arange(0,361,360/N)
+radiansAzimuths = [math.radians(degrees) for degrees in azimuths]
+pylab.rcParams['figure.figsize']=(5,5)
+for order in ordersToShow :
+	paternValues = numpy.array([sum(polarPattern(azimuth, order))/(order+1) for azimuth in radiansAzimuths])
+	paternValues /= max(paternValues)
+	pylab.polar(radiansAzimuths, paternValues, label='Order %s'%order )
+pylab.rgrids(numpy.arange(.4,1,.2),angle=220)
+pylab.legend(loc=2)
+pylab.title(title,horizontalalignment='center', verticalalignment='baseline', position=(.5,-.13))
+pylab.savefig(figurePath(__file__,"-"+decodingName,"pdf"))
+pylab.show()
+
+
