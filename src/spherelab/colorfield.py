cf25840492179e46b8dfff5363c8bd873182fbca
David García Garzón <voki@canvoki.net>
2012-08-31 16:38:35 +0200

    colorfield: Fixed line stride on 8bit maps

--- src/spherelab/colorfield.py
+++ src/spherelab/colorfield.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
-from PySide import QtCore, QtGui, QtOpenGL
-import sys
+from PySide import QtCore, QtGui
 import numpy as np
 import time
 
@@ -14,20 +13,36 @@ class ColorField(QtGui.QWidget) :
 		QtGui.qRgb(0xff-i,i,0) for i in xrange(0,256,4) ] + [
 		QtGui.qRgb(0,0xff-i,i) for i in xrange(0,256,4) ] + [
 		QtGui.qRgb(i,i,0xff) for i in xrange(0,256,4) ]
+	signedScale = [
+		QtGui.qRgb(255-2*i,12,12) for i in xrange(0,125) ] + [
+		QtGui.qRgb(2,0x2f,0)  ] + [
+		QtGui.qRgb(1,0x6f,0)  ] + [
+		QtGui.qRgb(0,0xff,0)  ] + [
+		QtGui.qRgb(0,0x6f,1)  ] + [
+		QtGui.qRgb(0,0x2f,2)  ] + [
+		QtGui.qRgb(12,12,2*(i-130)) for i in xrange(130,256) ]
 
 	def __init__(self, width, height, colorTable = None) :
 		super(ColorField, self).__init__()
-		self._colorTable = colorTable
 		self.resize(width, height)
-		qformat, npformat = (
-				QtGui.QImage.Format_Indexed8, np.uint8
+		self.format(width, height, colorTable)
+
+	def format(self, width, height, colorTable = None) :
+		self._colorTable = colorTable
+		qformat, npformat, pixelBytes = (
+				QtGui.QImage.Format_Indexed8, np.uint8, 1
 			) if colorTable else (
-				QtGui.QImage.Format_RGB32, np.uint32
+				QtGui.QImage.Format_RGB32, np.uint32, 4
 			)
 
 		self._img = QtGui.QImage(width, height, qformat)
 		if colorTable : self._img.setColorTable(colorTable)
-		self._rawdata = np.ndarray(shape=(height,width), dtype=npformat, buffer=self._img.bits())
+
+		self._rawdata = np.ndarray(
+			shape=(height,self._img.bytesPerLine()/pixelBytes),
+			dtype=npformat,
+			buffer=self._img.bits())[:,:width]
+
 
 	def data(self) :
 		return self._rawdata
@@ -51,21 +66,22 @@ class Reloader(QtCore.QObject) :
 		data = self._target.data()
 		data[:] = np.random.randint(0,0x00ffffff,size=data.shape).astype(data.dtype) # 12m
 #		data[:] = 0x00ff0000
-#		data[self._counter:self._counter+1,40:100] = 0x0000ff00
-		data[:256,-4] = np.arange(256)
-		data[:256,-3] = np.arange(256)
-		data[:256,-2] = np.arange(256)
-		data[:256,-1] = np.arange(256)
+		data[self._counter:self._counter+1,10:-10] = 0x0000ff00
+#		data[:256,-4] = np.arange(256)
+#		data[:256,-3] = np.arange(256)
+#		data[:256,-2] = np.arange(256)
+#		data[:256,-1] = np.arange(256)
 		self._counter += 1
 		self._target.reload()
 		period = 200
 		if not self._counter%period :
 			toc = time.clock()
-			print "%.2f frames/s"% (period/float(toc - self._tic)), "%2.2f ms"% (float(toc - self._tic)*1000/period)
+			print "%.2f frames/s"% (period/float(toc - self._tic) if toc!=self._tic else 0), "%2.2f ms"% (float(toc - self._tic)*1000/period)
 			self._tic = toc
 
 if __name__ == "__main__" :
 
+	import sys
 	width = 600
 	height = 400
 
