fbbc7aedd36a822bb020c9cd0b90d81faa6f073c
David García Garzón <voki@canvoki.net>
2012-10-11 17:03:15 +0200

    python-wavefile -> python-sndfile back, keep log

---
+++ src/python-sndfile/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from distutils.core import setup, Extension
+
+setup(
+	name = "wavefile",
+	version = "1.0",
+	description = "Pythonic wave file reader and writer",
+	author = "David Garcia Garzon",
+	author_email = "voki@canvoki.net",
+	url = 'https://github.com/vokimon/thesis/tree/master/src/python-sndfile',
+	packages=[
+		'wavefile',
+		],
+	)
+
